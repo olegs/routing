42fd65ff89f914efc5d8d04adcb7b4efdb0d106e
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2011-05-28 00:06:03 +0400

    Correct implementation of equals and hashcode for Pair

--- src/org/vika/routing/Pair.java
+++ src/org/vika/routing/Pair.java
@@ -1,5 +1,7 @@
 package org.vika.routing;
 
+import com.sun.istack.internal.NotNull;
+
 /**
  * @author oleg
  */
@@ -8,16 +10,28 @@ public class Pair<A, B> {
     public final B snd;
 
 
-    public Pair(final A fst, final B snd) {
+    public Pair(@NotNull final A fst, @NotNull final B snd) {
         this.fst = fst;
         this.snd = snd;
     }
 
-    public A getFirst(){
-        return fst;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+
+        Pair pair = (Pair) o;
+
+        if (!fst.equals(pair.fst)) return false;
+        if (!snd.equals(pair.snd)) return false;
+
+        return true;
     }
 
-    public B getSecond() {
-        return snd;
+    @Override
+    public int hashCode() {
+        int result = fst.hashCode();
+        result = 31 * result + snd.hashCode();
+        return result;
     }
 }
